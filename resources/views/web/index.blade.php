8b3d8f0aaf9defef6af795bd7aa17b4494467447
Cuchin2 <luis.lucho3210@gmail.com>
2023-12-16 19:35:21 -0500

    chilca

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 <div class="h-[1000px] text-gris-90 md:mt-[84px] mt-[44px]">
-<div class="w-full">
-    <img src="{{ asset('image/lodomens/Banner_Carrusel_1.png') }}" alt="Lodomens">
+<div class="">
+    <img src="{{ asset('image/lodomens/Banner_Carrusel_1.png') }}" class="w-full" alt="Lodomens">
 </div>
 <div class="grid md:grid-cols-2  col-span-1">
     <div class="col-12 md:col-6">
-        <img src="{{ asset('image/lodomens/Banner_1.png') }}" alt="" srcset="">
+        <img src="{{ asset('image/lodomens/Banner_1.png') }}" class="w-full" alt="" srcset="">
     </div>
     <div class="col-12 md:col-6">
-        <img src="{{ asset('image/lodomens/Banner_2.png') }}" alt="" srcset="">
+        <img src="{{ asset('image/lodomens/Banner_2.png') }}" class="w-full" alt="" srcset="">
     </div>
 </div>
 </div>
